dbc5f3c898c322f3d0708a3a9dbf3e4c8b0c6195
Julian Pelizäus <jp@adformc.de>
2025-02-21 00:45:34 +0100

    command: Add NewCommandFromString
    Signed-off-by: Julian Pelizäus <jp@adformc.de>

--- command/command.go
+++ command/command.go
@@ -5,11 +5,11 @@ import (
 	"strings"
 )
 
-type OpCode rune
+type OpCode string
 
 const (
-	OpCodeCabSpeed    OpCode = 't'
-	OpCodeCabFunction OpCode = 'F'
+	OpCodeCabSpeed    OpCode = "t"
+	OpCodeCabFunction OpCode = "F"
 )
 
 type Command struct {
@@ -26,24 +26,40 @@ func NewCommand(opCode OpCode, format string, parameters ...any) *Command {
 	}
 }
 
-func ParseRaw(command string) (string, error) {
-	oneLine := strings.Trim(command, "\n")
-	if len(oneLine) < 3 {
-		return "", fmt.Errorf("Invalid command: %q", command)
+func NewCommandFromString(command string) (*Command, error) {
+	if len(command) < 3 {
+		return nil, fmt.Errorf("Invalid command: %q", command)
 	}
 
-	return oneLine, nil
+	commandTrimmed := strings.Trim(command, "<>")
+	commandSplit := strings.Split(commandTrimmed, " ")
+	if len(commandSplit) < 1 {
+		return nil, fmt.Errorf("Op code missing in command: %q", command)
+	}
+
+	formatStr := make([]string, 0, len(commandSplit)-1)
+	parameters := make([]any, 0, len(commandSplit)-1)
+	for _, parameter := range commandSplit[1:] {
+		formatStr = append(formatStr, "%s")
+		parameters = append(parameters, parameter)
+	}
+
+	return &Command{
+		opCode:     OpCode(commandSplit[0]),
+		format:     strings.Join(formatStr, " "),
+		parameters: parameters,
+	}, nil
 }
 
-func (c *Command) StringRaw() string {
+func (c *Command) String() string {
 	// If no format is provided just print the op code.
 	if c.format == "" {
-		return fmt.Sprintf("<%c>", c.opCode)
+		return fmt.Sprintf("<%s>", c.opCode)
 	}
 
-	return fmt.Sprintf(fmt.Sprintf("<%c %s>", c.opCode, c.format), c.parameters...)
+	return fmt.Sprintf(fmt.Sprintf("<%s %s>", c.opCode, c.format), c.parameters...)
 }
 
 func (c *Command) Bytes() []byte {
-	return []byte(fmt.Sprintf("%s\n", c.StringRaw()))
+	return []byte(fmt.Sprintf("%s\n", c.String()))
 }
