d9b6e2cdbcff5c98f7ededbc47c114e252dac4ee
Julian Pelizäus <jp@adformc.de>
2025-02-23 20:55:07 +0100

    station: Add Ready and Status functions
    Signed-off-by: Julian Pelizäus <jp@adformc.de>

--- station/station.go
+++ station/station.go
@@ -23,6 +23,13 @@ const (
 	TrackJoin Track = "JOIN"
 )
 
+type Status struct {
+	Version             string
+	MicroprocessorType  string
+	MotorcontrollerType string
+	BuildNumber         string
+}
+
 type CommandStation struct {
 	protocol protocol.ReadWriteCloser
 }
@@ -37,19 +44,71 @@ func (s PowerState) OpCode() command.OpCode {
 	return command.OpCode(s)
 }
 
+// Console returns a channel and writer which can be used to retrieve and send
+// commands from and to the command station.
 func (c *CommandStation) Console() (protocol.CommandC, protocol.WriteF, protocol.CleanupF) {
 	commandC, cleanupF := c.protocol.Read()
 	return commandC, c.protocol.Write, cleanupF
 }
 
+// Power sets the power to the given state.
 func (c *CommandStation) Power(state PowerState) error {
 	return c.protocol.Write(command.NewCommand(state.OpCode(), ""))
 }
 
+// PowerTrack sets the tracks power to the given state.
 func (c *CommandStation) PowerTrack(state PowerState, track Track) error {
 	return c.protocol.Write(command.NewCommand(state.OpCode(), "%s", track))
 }
 
+// Ready waits for the <@ 0 3 "Ready"> broadcast message which indicates the station is ready the receive commands.
+func (c *CommandStation) Ready(ctx context.Context) error {
+	readyCommand := command.NewCommand(command.OpCodeInfo, "%d %d %q", 0, 3, "Ready")
+	observationC, cleanupF := c.protocol.ReadCommand(readyCommand)
+	defer cleanupF()
+
+	select {
+	case <-observationC:
+		return nil
+	case <-ctx.Done():
+		return ctx.Err()
+	}
+}
+
+// Status returns DCC-EX version and hardware info, along with defined turnouts.
+func (c *CommandStation) Status(ctx context.Context) (*Status, error) {
+	commandC, cleanupF := c.protocol.ReadOpCode(command.OpCodeStatusResponse)
+	defer cleanupF()
+
+	go func() {
+		_ = c.protocol.Write(command.NewCommand(command.OpCodeStatus, ""))
+	}()
+
+	select {
+	case statusCommand := <-commandC:
+		parameters, err := statusCommand.ParametersStrings()
+		if err != nil {
+			return nil, err
+		}
+
+		if len(parameters) != 7 {
+			return nil, fmt.Errorf("invalid command: %q", statusCommand.String())
+		}
+
+		status := &Status{
+			Version:             parameters[1],
+			MicroprocessorType:  parameters[3],
+			MotorcontrollerType: parameters[5],
+			BuildNumber:         parameters[6],
+		}
+
+		return status, nil
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	}
+}
+
+// SupportedCabs returns the number of supported cabs.
 func (c *CommandStation) SupportedCabs(ctx context.Context) (int, error) {
 	commandC, cleanupF := c.protocol.ReadOpCode(command.OpCodeStationSupportedCabs)
 	defer cleanupF()
