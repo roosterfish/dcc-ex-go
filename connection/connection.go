95ad095d566cfad534be2de8d78d09a941009b1b
Julian Pelizäus <jp@adformc.de>
2025-03-06 23:42:29 +0100

    connection: Use channels

--- connection/connection.go
+++ connection/connection.go
@@ -5,6 +5,7 @@ import (
 	"io"
 
 	"github.com/roosterfish/dcc-ex-go/cab"
+	"github.com/roosterfish/dcc-ex-go/channel"
 	"github.com/roosterfish/dcc-ex-go/protocol"
 	"github.com/roosterfish/dcc-ex-go/sensor"
 	"github.com/roosterfish/dcc-ex-go/station"
@@ -23,8 +24,8 @@ type Config struct {
 }
 
 type Connection struct {
-	config   *Config
-	protocol protocol.ReadWriteCloser
+	config  *Config
+	channel *channel.Channel
 }
 
 var DefaultMode Mode = &serial.Mode{
@@ -44,17 +45,24 @@ func NewConnection(config *Config) (*Connection, error) {
 		config: config,
 	}
 
+	// Open up a new serial connection.
 	port, err := conn.open()
 	if err != nil {
 		return nil, err
 	}
 
-	conn.protocol = protocol.NewProtocol(port, &protocol.Config{
+	// Wrap the serial connection with the protocol utilities.
+	connectionProtocol := protocol.NewProtocol(port, &protocol.Config{
 		RequireSubscriber: config.RequireSubscriber,
 	})
+
+	// Expose the protocol utilities using a channel.
+	// The channel offers various entities to interact with the underlying serial connection.
+	conn.channel = channel.NewChannel(connectionProtocol)
 	return conn, nil
 }
 
+// open tries to open up a new serial connection using the given device.
 func (c *Connection) open() (io.ReadWriteCloser, error) {
 	port, err := serial.Open(c.config.Device, c.config.Mode)
 	if err != nil {
@@ -65,17 +73,19 @@ func (c *Connection) open() (io.ReadWriteCloser, error) {
 }
 
 func (c *Connection) Cab(address cab.Address) *cab.Cab {
-	return cab.NewCab(address, c.protocol)
+	return cab.NewCab(address, c.channel)
 }
 
 func (c *Connection) Sensor(id sensor.ID) *sensor.Sensor {
-	return sensor.NewSensor(id, c.protocol)
+	return sensor.NewSensor(id, c.channel)
 }
 
 func (c *Connection) CommandStation() *station.CommandStation {
-	return station.NewStation(c.protocol)
+	return station.NewStation(c.channel)
 }
 
 func (c *Connection) Close() error {
-	return c.protocol.Close()
+	return c.channel.Session(func(protocol protocol.ReadWriteCloser) error {
+		return protocol.Close()
+	})
 }
