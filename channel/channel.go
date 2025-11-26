d137d795e56e61c6d4e92095b427d53ce68aae13
Julian Pelizäus <jp@adformc.de>
2025-11-26 01:11:24 +0100

    channel: Add SessionSuccess func

--- channel/channel.go
+++ channel/channel.go
@@ -1,9 +1,13 @@
 package channel
 
 import (
+	"context"
+	"fmt"
 	"sync"
 
+	"github.com/roosterfish/dcc-ex-go/command"
 	"github.com/roosterfish/dcc-ex-go/protocol"
+	"golang.org/x/sync/errgroup"
 )
 
 type Channel struct {
@@ -27,6 +31,47 @@ func (c *Channel) Session(sessionF func(protocol protocol.ReadWriteCloser) error
 	defer c.sessionLock.Unlock()
 
 	return sessionF(c.protocol)
+}
+
+// SessionSuccess is equal to Session including an additional reader that checks for the failure op code.
+// If any invalid command is sent throughout the session this likely indicates a wrong command was sent.
+// An example is examining the status of an invalid turnout.
+func (c *Channel) SessionSuccess(ctx context.Context, sessionF func(ctx context.Context, protocol protocol.ReadWriteCloser) error) error {
+	c.sessionLock.Lock()
+	defer c.sessionLock.Unlock()
+
+	// Allows to early cancel all other routines in the errgroup.
+	// This is required in case the sessionF returned with success so we can also cancel the failure op code reader.
+	earlyCancelCtx, earlyCancel := context.WithCancel(ctx)
+
+	g, ctx := errgroup.WithContext(earlyCancelCtx)
+	g.Go(func() error {
+		_, err := c.protocol.ReadOpCode(ctx, command.OpCodeFail)
+		if err != nil {
+			return err
+		}
+
+		return fmt.Errorf("observed failure during session")
+	})
+
+	g.Go(func() error {
+		err := sessionF(ctx, c.protocol)
+		if err == nil {
+			// Cancel the failure op code reader.
+			earlyCancel()
+		}
+
+		return err
+	})
+
+	err := g.Wait()
+
+	// Only return an error if it isn't caused by an early context cancellation.
+	if err != nil && earlyCancelCtx.Err() == nil {
+		return err
+	}
+
+	return nil
 }
 
 // RSession allows having a short-term read-only session on the connection's channel to interact
