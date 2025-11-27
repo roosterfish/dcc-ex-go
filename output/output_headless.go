db25b495000fd072c8223a36c4b4a1a725eafe01
Julian Pelizäus <jp@adformc.de>
2025-11-27 01:01:35 +0100

    output: Add OutputHeadless for direct output access

---
+++ output/output_headless.go
@@ -0,0 +1,58 @@
+package output
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/roosterfish/dcc-ex-go/channel"
+	"github.com/roosterfish/dcc-ex-go/command"
+	"github.com/roosterfish/dcc-ex-go/protocol"
+)
+
+type VPin uint16
+type AnalogValue uint16
+type Profile uint16
+type Duration uint16
+
+const (
+	ProfileNoPowerOff Profile = 0x80
+
+	LEDLow  AnalogValue = 0
+	LEDHigh AnalogValue = 4095
+)
+
+type OutputHeadless struct {
+	channel *channel.Channel
+}
+
+// NewOutputHeadless returns an output without ID.
+// It allows directly interacting with vPINs.
+func NewOutputHeadless(channel *channel.Channel) *OutputHeadless {
+	return &OutputHeadless{
+		channel: channel,
+	}
+}
+
+// SetAnalog sets the analog value to vPin using profile.
+func (o *OutputHeadless) SetAnalog(vPin VPin, value AnalogValue, profile Profile) error {
+	return o.channel.Session(func(protocol protocol.ReadWriteCloser) error {
+		err := protocol.Write(command.NewCommand(command.OpCodeOutputControl, "%d %d %d", vPin, value, profile))
+		if err != nil {
+			return fmt.Errorf("failed to set analog value on vpin %d: %w", vPin, err)
+		}
+
+		return nil
+	})
+}
+
+// SetAnalogDuration sets the analog value to vPin using profile and duration.
+func (o *OutputHeadless) SetAnalogDuration(vPin VPin, value AnalogValue, profile Profile, duration time.Duration) error {
+	return o.channel.Session(func(protocol protocol.ReadWriteCloser) error {
+		err := protocol.Write(command.NewCommand(command.OpCodeOutputControl, "%d %d %d %d", vPin, value, profile, duration.Milliseconds()/100))
+		if err != nil {
+			return fmt.Errorf("failed to set analog value on vpin %d over duration %q: %w", vPin, duration.String(), err)
+		}
+
+		return nil
+	})
+}
