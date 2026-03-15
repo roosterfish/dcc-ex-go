be01e25f1277eef95ef00234d54e2c6de8b7273a
Julian Pelizäus <jp@adformc.de>
2026-03-15 20:24:12 +0100

    cab: Use session abstraction and invert direction to resemble upstream behavior
    Signed-off-by: Julian Pelizäus <jp@adformc.de>

--- cab/cab.go
+++ cab/cab.go
@@ -1,15 +1,20 @@
 package cab
 
 import (
+	"context"
+	"fmt"
+	"strconv"
+
 	"github.com/roosterfish/dcc-ex-go/channel"
 	"github.com/roosterfish/dcc-ex-go/command"
 	"github.com/roosterfish/dcc-ex-go/protocol"
+	"golang.org/x/sync/errgroup"
 )
 
 // CabDirection can be either 0 or 1.
 type Direction uint8
 
-// Speed can be -1 or 0-127.
+// Speed can be -1 (emergency stop) or 0-127.
 type Speed int8
 type Address uint16
 type Function uint8
@@ -22,9 +27,14 @@ type Cab struct {
 	channel *channel.Channel
 }
 
+type CabStatus struct {
+	SpeedByte uint8
+	FunctMap  uint8
+}
+
 const (
-	DirectionForward Direction = iota
-	DirectionBackward
+	DirectionBackward Direction = iota
+	DirectionForward
 )
 
 const (
@@ -51,14 +61,110 @@ func NewCab(address Address, channel *channel.Channel) *Cab {
 	}
 }
 
-func (c *Cab) Speed(speed Speed, direction Direction) error {
-	return c.channel.Session(func(protocol protocol.ReadWriteCloser) error {
-		return protocol.Write(command.NewCommand(command.OpCodeCabSpeed, "%d %d %d", c.address, speed, direction))
-	})
+func (c *Cab) equalsCommandParams(params []string) bool {
+	return len(params) == 4 && params[0] == strconv.FormatUint(uint64(c.address), 10)
 }
 
-func (c *Cab) Function(funct Function, state FunctionState) error {
-	return c.channel.Session(func(protocol protocol.ReadWriteCloser) error {
-		return protocol.Write(command.NewCommand(command.OpCodeCabFunction, "%d %d %d", c.address, funct, state))
+func (c *Cab) speedUnchanged(status *CabStatus, newSpeed Speed, newDirection Direction) bool {
+	// 1: Backward emergency stop
+	// 129: Forward emergency stop
+	if status.SpeedByte == 1 && newSpeed == -1 && newDirection == DirectionBackward {
+		return true
+	} else if status.SpeedByte == 129 && newSpeed == -1 && newDirection == DirectionForward {
+		return true
+	}
+
+	// 2-127: Backward 1-126
+	// 130-255: Forward 1-126
+	if status.SpeedByte >= 2 && status.SpeedByte <= 127 {
+		// Check if already at the same speed and going backward.
+		if status.SpeedByte-1 == uint8(newSpeed) && newDirection == DirectionBackward {
+			return true
+		}
+	} else if status.SpeedByte >= 130 {
+		// Check if already at the same speed and going forward.
+		if status.SpeedByte-129 == uint8(newSpeed) && newDirection == DirectionForward {
+			return true
+		}
+	} else if status.SpeedByte == 0 && uint8(newSpeed) == 0 && newDirection == DirectionBackward {
+		// Stopped backward.
+		return true
+	} else if status.SpeedByte == 128 && uint8(newSpeed) == 0 && newDirection == DirectionForward {
+		// Stopped forward.
+		return true
+	}
+
+	return false
+}
+
+// Speed sets the cabs speed and direction.
+// It first checks whether or not the speed and direction is already set.
+// Keep in mind that the check and change are not inside the same session.
+func (c *Cab) Speed(ctx context.Context, speed Speed, direction Direction) error {
+	// Check if already at the requested speed.
+	// There isn't a broadcast sent if the cab is already at the requested speed and direction.
+	status, err := c.Status(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get status of cab %q: %w", c.address, err)
+	}
+
+	if c.speedUnchanged(status, speed, direction) {
+		return nil
+	}
+
+	speedCommand := command.NewCommand(command.OpCodeCabSpeed, "%d %d %d", c.address, speed, direction)
+	return c.channel.WriteAndReadOpCode(ctx, speedCommand, command.OpCodeCabResponse, c.equalsCommandParams)
+}
+
+func (c *Cab) Function(ctx context.Context, funct Function, state FunctionState) error {
+	functionCommand := command.NewCommand(command.OpCodeCabFunction, "%d %d %d", c.address, funct, state)
+	return c.channel.WriteAndReadOpCode(ctx, functionCommand, command.OpCodeCabResponse, c.equalsCommandParams)
+}
+
+func (c *Cab) Status(ctx context.Context) (*CabStatus, error) {
+	var responseCommand *command.Command
+	err := c.channel.SessionSuccess(ctx, func(ctx context.Context, protocol protocol.ReadWriteCloser) error {
+		g, ctx := errgroup.WithContext(ctx)
+
+		g.Go(func() error {
+			var err error
+			responseCommand, err = protocol.ReadOpCode(ctx, command.OpCodeCabResponse)
+			return err
+		})
+
+		g.Go(func() error {
+			return protocol.Write(command.NewCommand(command.OpCodeCabSpeed, "%d", c.address))
+		})
+
+		return g.Wait()
 	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to get status of cab %d: %w", c.address, err)
+	}
+
+	parameters, err := responseCommand.ParametersStrings()
+	if err != nil {
+		return nil, err
+	}
+
+	if len(parameters) != 4 {
+		return nil, fmt.Errorf("invalid command: %q", responseCommand.String())
+	}
+
+	speedByte, err := strconv.ParseUint(parameters[2], 10, 8)
+	if err != nil {
+		return nil, fmt.Errorf("invalid speed byte %q: %w", parameters[2], err)
+	}
+
+	functMap, err := strconv.ParseUint(parameters[3], 10, 16)
+	if err != nil {
+		return nil, fmt.Errorf("invalid funct map %q: %w", parameters[3], err)
+	}
+
+	status := &CabStatus{
+		SpeedByte: uint8(speedByte),
+		FunctMap:  uint8(functMap),
+	}
+
+	return status, nil
 }
