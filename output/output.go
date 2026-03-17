bd75fc39b9d48b9843bbb34d8be4637e3570cce0
Julian Pelizäus <jp@adformc.de>
2026-03-17 20:49:26 +0100

    output: Use general control command approach
    Signed-off-by: Julian Pelizäus <jp@adformc.de>

--- output/output.go
+++ output/output.go
@@ -34,7 +34,7 @@ func NewOutput(id ID, channel *channel.Channel) *Output {
 
 // Persist creates the output and persists its definition in the EEPROM.
 func (o *Output) Persist(ctx context.Context, vpin VPin, iFlag IFlag) error {
-	return o.channel.SessionSuccess(ctx, func(ctx context.Context, protocol protocol.ReadWriteCloser) error {
+	return o.channel.Session(func(protocol protocol.ReadWriteCloser) error {
 		err := protocol.Write(command.NewCommand(command.OpCodeOutput, "%d %d %d", o.id, vpin, iFlag))
 		if err != nil {
 			return fmt.Errorf("failed to create output: %w", err)
@@ -63,13 +63,18 @@ func (o *Output) setCommand(value DigitalValue) *command.Command {
 	return command.NewCommand(command.OpCodeOutput, "%d %c", o.id, value)
 }
 
-func (o *Output) equalsCommandParams(cmd *command.Command) (bool, error) {
+func (o *Output) equalsCommandParams(cmd *command.Command) error {
 	params, err := cmd.ParametersStrings()
 	if err != nil {
-		return false, fmt.Errorf("failed getting output command parameters: %w", err)
+		return fmt.Errorf("failed getting output command parameters: %w", err)
 	}
 
-	return len(params) == 2 && params[0] == strconv.FormatUint(uint64(o.id), 10), nil
+	outputMatch := len(params) == 2 && params[0] == strconv.FormatUint(uint64(o.id), 10)
+	if !outputMatch {
+		return fmt.Errorf("invalid response for output %d: %q", o.id, cmd.String())
+	}
+
+	return nil
 }
 
 func (o *Output) High(ctx context.Context) error {
@@ -82,69 +87,45 @@ func (o *Output) Low(ctx context.Context) error {
 
 func (o *Output) Status(ctx context.Context) (*Status, error) {
 	var outputStatus *Status
-	err := o.channel.Session(func(protocol protocol.ReadWriteCloser) error {
-		commandC, cleanupF := protocol.Read()
-		defer cleanupF()
 
-		g := &errgroup.Group{}
-		g.Go(func() error {
-			// Send a control command to allow waiting for the end of the output.
-			return protocol.Write(command.NewControlCommand(command.OpCodeOutput, ""))
-		})
-
-		err := g.Wait()
+	statusCommand := command.NewCommand(command.OpCodeOutput, "")
+	err := o.channel.WriteAndReadOpCode(ctx, statusCommand, command.OpCodeOutputResponse, func(cmd *command.Command) error {
+		params, err := cmd.ParametersStrings()
 		if err != nil {
-			return err
+			return fmt.Errorf("failed getting output command parameters: %w", err)
 		}
 
-		for {
-			select {
-			case cmd := <-commandC:
-				// Loop through the received commands until observing the end of the output.
-				// This is indicated by the fail opcode as we have sent an invalid control command.
-				if cmd.OpCode() == command.OpCodeFail {
-					return nil
-				}
-
-				if cmd.OpCode() == command.OpCodeOutputResponse {
-					params, err := cmd.ParametersStrings()
-					if err != nil {
-						continue
-					}
-
-					if len(params) != 4 {
-						continue
-					}
-
-					if params[0] != strconv.FormatUint(uint64(o.id), 10) {
-						continue
-					}
-
-					vPin, err := strconv.ParseUint(params[1], 10, 16)
-					if err != nil {
-						return fmt.Errorf("invalid vpin %q: %w", params[1], err)
-					}
-
-					iFlag, err := strconv.ParseUint(params[2], 10, 8)
-					if err != nil {
-						return fmt.Errorf("invalid iflag %q: %w", params[2], err)
-					}
-
-					state := []rune(params[3])
-					if len(state) != 1 {
-						return fmt.Errorf("invalid state %q", params[3])
-					}
-
-					outputStatus = &Status{
-						VPin:  VPin(vPin),
-						IFlag: IFlag(iFlag),
-						State: DigitalValue(state[0]),
-					}
-				}
-			case <-ctx.Done():
-				return ctx.Err()
-			}
+		if len(params) != 4 {
+			return fmt.Errorf("invalid output command parameter length %q", len(params))
 		}
+
+		if params[0] != strconv.FormatUint(uint64(o.id), 10) {
+			// Not the right output, return early.
+			return nil
+		}
+
+		vPin, err := strconv.ParseUint(params[1], 10, 16)
+		if err != nil {
+			return fmt.Errorf("invalid vpin %q: %w", params[1], err)
+		}
+
+		iFlag, err := strconv.ParseUint(params[2], 10, 8)
+		if err != nil {
+			return fmt.Errorf("invalid iflag %q: %w", params[2], err)
+		}
+
+		state := []rune(params[3])
+		if len(state) != 1 {
+			return fmt.Errorf("invalid state %q", params[3])
+		}
+
+		outputStatus = &Status{
+			VPin:  VPin(vPin),
+			IFlag: IFlag(iFlag),
+			State: DigitalValue(state[0]),
+		}
+
+		return nil
 	})
 	if err != nil {
 		return nil, fmt.Errorf("failed to get output status: %w", err)
