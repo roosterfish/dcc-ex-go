cb19dbb1e2b6f01dcb986c45d8532c3b98d0aeb7
Julian Pelizäus <jp@adformc.de>
2026-03-17 23:27:13 +0100

    channel: Update docs
    Signed-off-by: Julian Pelizäus <jp@adformc.de>

--- channel/channel.go
+++ channel/channel.go
@@ -1,46 +1,34 @@
 package channel
 
 import (
+	"context"
 	"sync"
 
 	"github.com/roosterfish/dcc-ex-go/command"
 	"github.com/roosterfish/dcc-ex-go/protocol"
 )
 
-// readWriteCloserCache wraps the protocols readWriteCloser and allows caching the written commands.
-type readWriteCloserCache struct {
-	protocol.ReadWriteCloser
-	commandCache command.Command
-}
-
-// Write caches the written command and calls the wrapped readWriteCloser's Write.
-func (r *readWriteCloserCache) Write(command *command.Command) error {
-	// Don't copy the pointer to ensure we don't influence the underlying protocol.
-	r.commandCache = *command
-	return r.ReadWriteCloser.Write(command)
-}
-
-// LastCommand returns the last written command from the cache.
-func (r *readWriteCloserCache) lastCommand() *command.Command {
-	return &r.commandCache
-}
+type WriteF func(ctx context.Context, command *command.Command) error
 
 type Channel struct {
-	protocol    *readWriteCloserCache
+	protocol    protocol.ReadWriteCloser
 	sessionLock sync.Mutex
 }
 
 // NewChannel returns a new channel using the given protocol.
 func NewChannel(protocol protocol.ReadWriteCloser) *Channel {
 	return &Channel{
-		protocol: &readWriteCloserCache{ReadWriteCloser: protocol},
+		protocol: protocol,
 	}
 }
 
-// Session allows having a short-term session on the connection's channel to interact
-// with the underlying protocol.
+// Consider using the channel abstraction functions instead as those perform additional control command handling to gate
+// the beginning and end of a session and can ensure that no response is leaked into follow-up sessions.
+//
+// Session allows having a short-term session on the connection's channel to interact with the underlying protocol.
 // There can only be a single session at a time.
 // Session is thread safe and allows exclusive read and write from and to the channel.
+// There can be other read sessions in parallel.
 func (c *Channel) Session(sessionF func(protocol protocol.ReadWriteCloser) error) error {
 	c.sessionLock.Lock()
 	defer c.sessionLock.Unlock()
@@ -48,10 +36,9 @@ func (c *Channel) Session(sessionF func(protocol protocol.ReadWriteCloser) error
 	return sessionF(c.protocol)
 }
 
-// RSession allows having a short-term read-only session on the connection's channel to interact
-// with the underlying protocol.
+// RSession allows having a short-term read-only session on the connection's channel to interact with the underlying protocol.
 // Unlike Session it only allows reading.
-// It allows multiple concurrent reader sessions independent whether or not there is an active rw session.
+// It allows multiple concurrent reader sessions independent whether or not there is an active read and write session.
 func (c *Channel) RSession(sessionF func(protocol protocol.Reader) error) error {
 	return sessionF(c.protocol)
 }
