52718cde383a2c37b5bd53438a2e0b56e55648e3
Julian Pelizäus <jp@adformc.de>
2026-03-17 20:48:04 +0100

    channel: Use new general control command approach
    Signed-off-by: Julian Pelizäus <jp@adformc.de>

--- channel/abstractions.go
+++ channel/abstractions.go
@@ -9,30 +9,39 @@ import (
 )
 
 // WriteAndReadOpCode abstracts an underlying read/write session by writing the given command and waiting for a response with the given op code.
-// Once the op code is observed, the given function f is called with the observed command.
-// It will continue to read commands until the function f returns true or the context is done.
-func (c *Channel) WriteAndReadOpCode(ctx context.Context, cmd *command.Command, o command.OpCode, f func(cmd *command.Command) (bool, error)) error {
+// Once the op code is observed, the given function f is called with the observed command(s).
+// It will continue to read commands until the function f returns an error, the context is cancelled or the control command is observed.
+// In case the function f returns an error, those are accumulated and only returned once
+func (c *Channel) WriteAndReadOpCode(ctx context.Context, cmd *command.Command, o command.OpCode, f func(cmd *command.Command) error) error {
 	return c.Session(func(protocol protocol.ReadWriteCloser) error {
 		commandC, cleanupF := protocol.Read()
 		defer cleanupF()
 
-		err := protocol.Write(cmd)
+		// Derive a new control command.
+		controlCommand := command.NewControlCommand(cmd.OpCode(), cmd.Format(), cmd.Parameters()...)
+		err := protocol.Write(controlCommand)
 		if err != nil {
 			return err
 		}
+
+		// When sending <X>, the command stations replies with <* Opcode=X params=0 *><X>.
+		describeCommandStr := command.NewCommand(command.OpCodeDescribe, "%s %s %s", "Opcode=X", "params=0", "*").String()
+		describeCommandObserved := false
 
 		for {
 			select {
 			case cmd := <-commandC:
 				if cmd.OpCode() == o {
-					ok, err := f(cmd)
+					err := f(cmd)
 					if err != nil {
 						return fmt.Errorf("failed to run function: %w", err)
 					}
-
-					if ok {
-						return nil
-					}
+				} else if cmd.String() == describeCommandStr {
+					// About to be done, waiting for <X>.
+					describeCommandObserved = true
+				} else if cmd.OpCode() == command.OpCodeFail && describeCommandObserved {
+					// <X> observed, return the session cleanly.
+					return nil
 				}
 			case <-ctx.Done():
 				return ctx.Err()
